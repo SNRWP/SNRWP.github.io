7a2e64bc88cdb410efc1012cb2e08cb76b956ab3
中国社会主义国民复兴工人党 <ethanoex@163.com>
2024-12-02 06:43:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// This script is optional if you want to add interactivity or dynamic content loading.
-document.addEventListener('DOMContentLoaded', function() {
-    const containers = document.querySelectorAll('.container');
-    containers.forEach(container => {
-        container.setAttribute('data-date', container.querySelector('h2').innerText);
-    });
-});
+
